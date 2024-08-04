6686ef22d9344160b01f969032a4b01a2ebbf4f8
Ayush Bhuskat <ayushbhuskat@gmail.com>
2024-08-04 23:37:57 +0530

    second commit

---
+++ input.py
@@ -0,0 +1,6 @@
+a = input("enter the first number : ") # it will treat the input like string and will concat it 
+b = input("enter the second number : ")# we have to convert it into integer by using typecasting first
+
+print(a + b)
+
+print( int(a) + int(b))
