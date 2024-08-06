9e8e2d1d62ec9eed88f91404cab5a9b2ad5df76b
Ayush Bhuskat <ayushbhuskat@gmail.com>
2024-08-06 14:17:16 +0530

    changes in strings

--- strings.py
+++ strings.py
@@ -14,3 +14,27 @@ print(name[0])#indexing in string
 for character in name : print(character)
 
 for character in apple : print(character)
+
+
+#slicing in strings
+
+fruit = "Mango"
+
+print(len(fruit))
+
+
+print(fruit[0:5])
+print(fruit[:5])
+print(fruit[0:len(fruit)-3])#-ve indexing
+print(fruit[-3:-1])
+
+
+
+#string methods
+#STRINGS ARE IMMUTABLE
+a = "ayush!!!!" 
+print(a.upper())
+print(a.rstrip("!"))#remove trailing character
+print(a.replace("ayush","AYUSH"))
+
+#some methods- split() , capitalize() , center() , count() , endswith() , find() , index() , isalnum() , isalpha() , isprintable(), isspace()
