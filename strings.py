8fd1ea5ce4502ea13ba1e0a7c7cd0c3cfbd021a2
Ayush Bhuskat <ayushbhuskat@gmail.com>
2024-08-06 13:44:46 +0530

    strings

---
+++ strings.py
@@ -0,0 +1,16 @@
+#multiline strings in python
+
+apple = '''hello,my name is ayush
+i am 20 year old and doing engineering 
+thank you'''
+
+print(apple)
+
+name = "ayush"
+
+print(name[0])#indexing in string
+
+
+for character in name : print(character)
+
+for character in apple : print(character)
